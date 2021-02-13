8299faf1a005f92f39bdb61b51fb3dfb2ea696b1
kvntl <kevinpatel4400@gmail.com>
2021-02-14 01:23:28 +0530

    new: add pick_objects and scripts packages

---
+++ src/scripts/pick_objects.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/robo-nd-HSR/src/map/world/test_map.world" &
+sleep 15
+xterm  -e  " roslaunch turtlebot_navigation amcl_demo.launch map_file:=/home/workspace/robo-nd-HSR/src/map/world/map.yaml" &
+sleep 10
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 10
+xterm  -e  " rosrun pick_objects pick_objects"
