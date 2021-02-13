fc852b6f3888bf51344bd5707fdcb1db1efdb1df
kvntl <kevinpatel4400@gmail.com>
2021-02-14 04:03:03 +0530

    new: final HSR script

---
+++ src/scripts/home_service.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+xterm  -e  " roslaunch my_robot world.launch" &
+sleep 20
+xterm  -e  " roslaunch my_robot amcl.launch map_file:=/home/workspace/robo_nd/src/map/myMap.yaml" &
+sleep 15
+xterm  -e  " rosrun rviz rviz -d /home/workspace/robo_nd/src/rvizConfig/rvizConfig.rviz" &
+sleep 15
+xterm  -e  " rosrun pick_objects pick_objects"
+sleep 15
+xterm  -e  " rosrun add_markers add_markers"
+sleep 15
