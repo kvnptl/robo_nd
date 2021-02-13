6b118d982537a6851b4ec5bc05d37ebbd3c46f42
kvntl <kevinpatel4400@gmail.com>
2021-02-14 04:46:30 +0530

    test

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 xterm  -e  " roslaunch my_robot world.launch" &
-sleep 20
+sleep 15
 xterm  -e  " roslaunch my_robot amcl.launch map_file:=/home/workspace/robo_nd/src/map/myMap.yaml" &
 sleep 15
 xterm  -e  " rosrun rviz rviz -d /home/workspace/robo_nd/src/rvizConfig/rvizConfig.rviz" &
-sleep 15
+sleep 10
 xterm  -e  " rosrun add_markers add_markers"
 xterm  -e  " rosrun pick_objects pick_objects"
