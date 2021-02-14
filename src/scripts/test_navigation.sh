d69598f663fbcdfc68f89507e84ff665ee70eb8e
kvntl <kevinpatel4400@gmail.com>
2021-02-14 15:49:25 +0530

    update: absolute path to relative path

--- src/scripts/test_navigation.sh
+++ src/scripts/test_navigation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xterm  -e  " roslaunch my_robot world.launch" &
 sleep 20
-xterm  -e  " roslaunch my_robot amcl.launch map_file:=/home/workspace/robo_nd/src/map/myMap.yaml" &
+xterm  -e  " roslaunch my_robot amcl.launch map_file:=$(rospack find map)/myMap.yaml" &
 sleep 15
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch"
