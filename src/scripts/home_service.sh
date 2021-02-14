5feb3e41a0666a753e00388f2945b4b50a601899
kvntl <kevinpatel4400@gmail.com>
2021-02-14 15:38:27 +0530

    modify: absolute path to relative path

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 xterm  -e  " roslaunch my_robot world.launch" &
 sleep 15
-xterm  -e  " roslaunch my_robot amcl.launch map_file:=/home/workspace/robo_nd/src/map/myMap.yaml" &
+xterm  -e  " roslaunch my_robot amcl.launch map_file:=$(rospack find map)/myMap.yaml" &
 sleep 15
-xterm  -e  " rosrun rviz rviz -d /home/workspace/robo_nd/src/rvizConfig/rvizConfig.rviz" &
+xterm  -e  " rosrun rviz rviz -d $(rospack find rvizConfig)/rvizConfig.rviz" &
 sleep 10
 xterm  -e  " rosrun add_markers add_markers" &
 sleep 1
