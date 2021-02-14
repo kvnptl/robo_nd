43dc557d969af0b44d2bf48c336125b0ebc576b1
kvntl <kevinpatel4400@gmail.com>
2021-02-14 15:55:28 +0530

    modify: pkg path

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -3,7 +3,7 @@ xterm  -e  " roslaunch my_robot world.launch" &
 sleep 20
 xterm  -e  " roslaunch my_robot amcl.launch map_file:=$(rospack find map)/myMap.yaml" &
 sleep 15
-xterm  -e  " rosrun rviz rviz -d $(rospack find rvizConfig)/rvizConfig.rviz" &
+xterm  -e  " rosrun rviz rviz -d $(rospack find rviz_config)/rvizConfig.rviz" &
 sleep 10
 xterm  -e  " rosrun add_markers add_markers" &
 sleep 1
